69d233dcefffc303f3d0d58fb42ae53de1904d80
boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>
2020-12-18 11:56:17 +0100

    Update modNetwork.class.php
    remove user rights

--- core/modules/modNetwork.class.php
+++ core/modules/modNetwork.class.php
@@ -191,7 +191,7 @@ class modNetwork extends DolibarrModules
 
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'network_read';	// Permission label
-		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
